ddf0f9472897fcb566f49f5ccf70ba967f67f9ce
bohdan.herasymenko <bogdan.gerasymenko@agileengine.com>
2019-01-14 19:16:46 +0200

    Refactoring in progress

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import { Provider } from 'react-redux'
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
 
-import store from './redux/store'
-import { Navigation } from './scenes/navigation'
-import { Content } from './scenes/content'
+import store from './configuration/store'
+import NavBar from './containers/navbar'
+// import { Content } from './scenes/content'
 
 import * as serviceWorker from './serviceWorker';
 import './index.css'
@@ -15,8 +15,8 @@ ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <div>
-        <Navigation />
-        <Content />
+        <NavBar />
+        {/* <Content /> */}
       </div>
     </BrowserRouter>
   </Provider>,
--- src/redux/navbar/actions.js
+++ src/redux/navbar/actions.js
@@ -1,12 +1,66 @@
+import { actionTypes } from './constants'
+import { getNavigationStructure } from '../../utils/api'
+
 export function selectSection(section) {
   return {
-    type: 'SELECT_SECTION',
+    type: undefined,
     selectedSection: section
   }
 }
 
-export function getNavbarStructure() {
+export function navbarStructureRequested() {
   return {
-    type: 'GET_NAVBAR_STRUCTURE'
+    type: actionTypes.GET_NAVBAR_STRUCTURE_REQUESTED
+  }
+}
+
+export function navbarStructureReceived(structure) {
+  return {
+    type: actionTypes.GET_NAVBAR_STRUCTURE_COMPLETED,
+    structure: structure
+  }
+}
+
+export function getNavbarStructure() {
+  return function(dispatch) {
+    dispatch(navbarStructureRequested())
+    // return fetch(``)
+    let answer = new Promise((resolve, reject) => {
+      resolve()
+    })
+
+    answer
+      .then(
+        _ => [
+          {
+            label: "Home",
+            menu: [
+              "Home",
+              "Help Desk",
+              "Offices",
+            ]
+          },
+          {
+            label: "Sales",
+            menu: [
+              "Leads",
+              "Opportunities",
+              "Clients",
+            ]
+          },
+          {
+            label: "Operations",
+            menu: [
+              "Clients",
+              "Projects",
+              "Assignments",
+            ]
+          }
+        ],
+        error => console.log('An error occured', error)
+      )
+      .then(structure => dispatch(navbarStructureReceived(structure)))
+
+      return answer
   }
 }
